39778cb6a5e10469d25b3b637c134708f60918e5
Karthikeya Chaturvedula <karthikeyachaturvedula@gmail.com>
2018-05-07 16:50:42 +0530

    check commit

--- ChickenWorld/ChickenWorld/settings.py
+++ ChickenWorld/ChickenWorld/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'ChickenWorld.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_name',                      
-        'USER': 'db_user',
-        'PASSWORD': 'db_user_password',
-        'HOST': '',
-        'PORT': '15662',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_name',                      
+#         'USER': 'db_user',
+#         'PASSWORD': 'db_user_password',
+#         'HOST': '',
+#         'PORT': '15662',
+#     }
+# }
 
 
 # Password validation
